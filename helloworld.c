b12dcca9308e23ff7182ee7ebbf2602fcb065399
Bindup03 <134941886+Bindup03@users.noreply.github.com>
2023-05-31 14:40:17 +0530

    Update helloworld.c

--- helloworld.c
+++ helloworld.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
-
+#include <stdlib.h>
+#include <string.h>
 #include "helloworld.h"
 
-int main(int argc, char *argv[])
+int main()
 {
   printf("Hello Yocto Recipe World\n");
   return 0;
